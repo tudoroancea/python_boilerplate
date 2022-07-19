9d33a560b18012b1cc1fe00adda1580ce6a25085
tudoroancea <oancea.tudor@icloud.com>
2022-07-19 17:19:32 +0000

    removed init_workspace.sh

--- init_workspace.sh
+++
@@ -1,9 +0,0 @@
-python3 -m pip install --upgrade pip
-pip3 install -r requirements.txt
-pip3 install black
-pip3 install black[d]
-python3 -m ven venv
-source venv/bin/activate
-python3 -m pip install --upgrade pip
-pip3 install -e .
-deactivate

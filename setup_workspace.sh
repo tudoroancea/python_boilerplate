9f622a285e386e17ee6e15fcb57fab3b3671684b
Ted <oancea.tudor@icloud.com>
2022-09-04 12:12:20 +0200

    :bug: added missing installation for requirements.txt

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -1,12 +1,18 @@
 # Copyright (c) Tudor Oancea, 2022
 
 # create virtual environment (optional)
-python3 -m venv venv
+if [ -d "venv" ]
+then
+  echo "venv already exists"
+else
+  python3 -m venv venv
+fi
 source venv/bin/activate
 
 # install dependencies through pip
 pip3 install --upgrade pip
-pip3 install -r requirements-dev.txt
+pip3 install -r requirements.txt
+pip3 install -r requirements_dev.txt
 pip3 install -U black pre-commit  # install black and pre-commit if they were not listed in requirements-dev.txt
 pre-commit install
 
